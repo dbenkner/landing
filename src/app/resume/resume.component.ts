eab9dc69d00297e529a0fc65bd0383892160d12c
David Benkner <d.benkner@gmail.com>
2023-11-13 14:17:05 -0500

    Refactored resume data retrival

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Job } from './job';
+import { Job } from './job.class';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ResumeService } from './resume.service';
@@ -11,20 +11,14 @@ import { Education } from './education.class';
   styles: []
 })
 export class ResumeComponent {
-  jobs:Job[] = [
-    new Job("Schoening Technology Group under contract for Luoxottica", "Technical Support Cordniator", "Cincinnati, Oh", '2022-9', '2023-6', ['Responsible for providing support for over 5000 luxottica sites', 
-    'Troubleshot various pieces of in-store technology: Oracle Xstore POS Systems, store networks/internet connectivity, telecommunications equipment, computer hardware / peripherals, internally and externally developed applications',
-    'Correctly recorded all incidents into ticketing system', 'Gathered and processed users’ information via verbal communication and screen viewer software to properly resolve or escalate user issues',
-    'Successfully educated users on fixes and workarounds or set expectations for escalated issues']),
-    new Job("Startek", "Technical Support Representative", "Hamilton, Oh", '2019-1', '2022-8', [])
-  ];
+  jobs:Job[] = [];
   url: string = '/assets/skills.json';
   skills:any;
   education:Education[] = [];
   constructor(private http: HttpClient,
     private resumeSvc: ResumeService) {}
   ngOnInit() {
-    this.resumeSvc.getSkills().subscribe(
+    this.resumeSvc.getJsonData('skills').subscribe(
       res => {
         this.skills = Object.keys(res).map(a=>res[a]);
         console.log(this.skills);
@@ -33,7 +27,7 @@ export class ResumeComponent {
         }
       }
     );
-    this.resumeSvc.getEducation().subscribe(
+    this.resumeSvc.getJsonData('education').subscribe(
       {
         next: (res) => {
           this.education = res;
@@ -41,6 +35,15 @@ export class ResumeComponent {
         },
         error: (err) => {
           console.error(err);
+        }
+      }
+    );
+    this.resumeSvc.getJsonData('jobs').subscribe(
+      {
+        next: (res) => {
+          console.debug(res);
+          this.jobs = res;
+          console.debug(this.jobs);
         }
       }
     )
--- src/app/resume/resume.service.ts
+++ src/app/resume/resume.service.ts
@@ -10,10 +10,7 @@ export class ResumeService {
 
     constructor(private http: HttpClient) {}
     
-    getSkills():Observable<any> {
-      return this.http.get('/assets/skills.json') as Observable<any>;
-    }
-    getEducation():Observable<Education[]> {
-      return this.http.get('/assets/education.json') as Observable<Education[]>;
+    getJsonData(data:string):Observable<any> {
+      return this.http.get(`/assets/${data}.json`) as Observable<any>;
     }
 }
